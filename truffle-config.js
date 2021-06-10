3e321f5ce9793d95ed9d9646feadca955e9a0fcd
Aniket Shaha <aniketshaha2001@gmail.com>
2021-06-10 17:34:23 +0530

    Fine-tunings in infura

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,11 @@
 require('babel-register')
 require('babel-polyfill')
 require('dotenv').config()
-const HDWalletProvider = require('truffle-hdwallet-provider-privkey')
-const privateKeys = process.env.PRIVATE_KEYS || ''
+const HDWalletProvider = require('truffle-hdwallet-provider')
+//const privateKeys = process.env.PRIVATE_KEYS || ''
+const mnemonic =
+  'tag bleak scout inflict volcano have more lens ripple island furnace manage'
+// const infuraKey = process.env.INFURA_API_KEY
 
 module.exports = {
   networks: {
@@ -11,7 +14,7 @@ module.exports = {
       port: 7545,
       network_id: '*', // Match any network id
     },
-    kovan: {
+    /* kovan: {
       provider: function () {
         return new HDWalletProvider(
           privateKeys.split(','), // Array of account private keys
@@ -21,8 +24,8 @@ module.exports = {
       gas: 5000000,
       gasPrice: 5000000000, // 5 gwei
       network_id: 42,
-    },
-    main: {
+    }, */
+    /* main: {
       provider: function () {
         return new HDWalletProvider(
           privateKeys.split(','), // Array of account private keys
@@ -43,17 +46,19 @@ module.exports = {
       gas: 5000000,
       gasPrice: 5000000000, // 5 gwei
       network_id: 4,
-    },
+    }, */
     ropsten: {
       provider: function () {
         return new HDWalletProvider(
-          privateKeys.split(','), // Array of account private keys
-          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}` // Url to an Ethereum Node
+          mnemonic, // Array of account private keys
+          'https://ropsten.infura.io/v3/33fe33cf053247a6a357a2fb37bb1b78' // Url to an Ethereum Node
         )
       },
-      gas: 5000000,
-      gasPrice: 5000000000, // 5 gwei
+      from: '0x1eDf41CB9Cb2c6d26008F757E7533F532C14F5b0',
+      gas: 5500000,
+      gasPrice: 25000000000, // 5 gwei
       network_id: 3,
+      // timeoutBlocks: 200,
     },
   },
   contracts_directory: './src/contracts/',
